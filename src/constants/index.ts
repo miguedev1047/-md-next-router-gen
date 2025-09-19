db9ae53aacf519e2fe979a2435d6bee4f2aca6e4
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-09-18 20:17:11 -0500

    FEAT: add constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,16 @@
+export const FILES_ON_VIEW = [
+  'page.tsx',
+  'layout.tsx',
+  'template.tsx',
+  'default.tsx',
+  'route.ts',
+  'loading.tsx',
+  'error.tsx',
+  'page.jsx',
+  'layout.jsx',
+  'template.jsx',
+  'default.jsx',
+  'route.js',
+  'loading.jsx',
+  'error.jsx',
+]
